5ec003f6d8f844caf707a9359474d93bcfa2c347
Giovanni Martina <gio@popado.co>
2017-01-25 19:04:40 -0200

    Add placeholder kanban board component

--- src/components/kanban-app.vue
+++ src/components/kanban-app.vue
@@ -7,17 +7,20 @@
 <template>
     <div id="kanban-container">
         <kanban-header></kanban-header>
+        <kanban-board></kanban-board>
         <kanban-footer></kanban-footer>
     </div>
 </template>
 
 <script>
 import KanbanHeader from './kanban-header.vue'
+import KanbanBoard from './kanban-board.vue'
 import KanbanFooter from './kanban-footer.vue'
 
 export default {
     components: {
         KanbanHeader,
+        KanbanBoard,
         KanbanFooter
     }
 }
---
+++ src/components/kanban-board.vue
@@ -0,0 +1,12 @@
+<!-- 
+    kanban-board
+
+    The actual Kanban board parent container
+-->
+<template>
+    <main id="kanban-board">
+        da board
+    </main>
+</template>
+
+<style lang="scss" src="./kanban-board.scss"></style>
