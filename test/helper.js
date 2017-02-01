66f978b60f6649e5245558ab67354a20ad17ec49
Giovanni Martina <gio@popado.co>
2017-02-01 13:22:15 -0200

    Clarify purpose of helper.js

--- test/helper.js
+++ test/helper.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 
 /* a helper function to subclass a Vue component and mount
  * with given propsData
+ * 
+ * used for testing Vue components
  */
 export default function getViewModel(Component, propsData) {
     const Ctor = Vue.extend(Component)
